82983fc2e868d0fdd447fd6dd56d08f4139fbe9a
Jenil <bhalalajenil47@gmail.com>
2023-02-10 00:49:30 +0530

    searching functionality added

--- js/showProducts.js
+++ js/showProducts.js
@@ -1,21 +1,63 @@
 const addBtnElement = document.querySelector('.add-product-btn');
 const sortByNameIdPriceElement = document.getElementById('sortby');
 const selectedSortingOrderElement = document.getElementById('sort-order');
+const searchElement = document.querySelector('.search');
 
 let sortOrderFlag = 0;
 let sortByFlag = 'id';
+let searchFlag = false;
+
+const debouncing = function (fn, delay=500){
+    let timer;
+    return function() {
+        clearTimeout(timer);
+
+        timer = setTimeout(()=>{
+            fn();
+        }, delay);
+    }
+}
+
+searchElement.addEventListener('keyup', () => {
+    searchFlag = true;
+    debouncing(showData, 600)();
+})
 
 const sortProductByIdAscendingOrder = () => {
-    console.log(product)
     productList.sort((a, b) => {
         return a.productId - b.productId;
+    });
+}
+
+const sortProductByIdDescendingOrder = () => {
+    productList.sort((a, b) => {
+        return b.productId - a.productId;
+    });
+}
+
+const sortProductByNameAscendingOrder = () => {
+    productList.sort((a, b) => a.productName.localeCompare(b.productName))
+}
+
+const sortProductByNameDescendingOrder = () => {
+    productList.sort((a, b) => b.productName.localeCompare(a.productName))
+}
+
+const sortProductByPriceAscendingOrder = () => {
+    productList.sort((a, b) => {
+        return a.productPrice - b.productPrice;
+    });
+}
+
+const sortProductByPriceDescendingOrder = () => {
+    productList.sort((a, b) => {
+        return b.productPrice - a.productPrice;
     });
 }
 
 
 sortByNameIdPriceElement.addEventListener('change', ()=>{
     if(sortByNameIdPriceElement.value == "sort-by-product-id"){
-        console.log("another")
         sortByFlag = 'id';
     }
     else if(sortByNameIdPriceElement.value == "sort-by-product-name"){
@@ -34,7 +76,6 @@ sortByNameIdPriceElement.addEventListener('change', ()=>{
 
 selectedSortingOrderElement.addEventListener('change', ()=>{
     if(selectedSortingOrderElement.value === "ascending"){
-        console.log("hello")
         sortOrderFlag = 1;
     }
     else if(selectedSortingOrderElement.value === "descending"){
@@ -48,17 +89,6 @@ selectedSortingOrderElement.addEventListener('change', ()=>{
 })
 
 
-
-// else if(selectedSortingOrderElement.value == "descending" && sortByNameOrIdElement.value == "sort-by-product-id"){
-//     sortProductByIdDescendingOrder();
-// }
-// else if(selectedSortingOrderElement.value == "ascending" && sortByNameOrIdElement.value == "sort-by-product-name"){
-//     sortProductByNameAscendingOrder();
-// }
-// else if(selectedSortingOrderElement.value == "descending" && sortByNameOrIdElement.value == "sort-by-product-name"){
-//     sortProductByNameDescendingOrder();
-// }
-
 const showData = () => {    
     if (localStorage.getItem('productArr') == null) {
         productList = [];
@@ -67,9 +97,38 @@ const showData = () => {
         productList = JSON.parse(localStorage.getItem('productArr'))
     }
 
+
+    if(searchFlag == true){
+        productList = productList.filter(obj => obj.productName.includes(searchElement.value));
+        
+    }
+
+
     if(sortOrderFlag === 1 && sortByFlag === 'id'){
+        console.log(1)
         sortProductByIdAscendingOrder();
     }
+    else if(sortOrderFlag === -1 && sortByFlag === 'id'){
+        console.log(2)
+        sortProductByIdDescendingOrder();
+    }
+    else if(sortOrderFlag === 1 && sortByFlag === 'name'){
+        console.log(3)
+        sortProductByNameAscendingOrder();
+    }
+    else if(sortOrderFlag === -1 && sortByFlag === 'name'){
+        console.log(4)
+        sortProductByNameDescendingOrder();
+    }
+    else if(sortOrderFlag === 1 && sortByFlag === 'price'){
+        console.log(5)
+        sortProductByPriceAscendingOrder();
+    }
+    else if(sortOrderFlag === -1 && sortByFlag === 'price'){
+        console.log(6)
+        sortProductByPriceDescendingOrder();
+    }
+
 
     var html = '';
 
